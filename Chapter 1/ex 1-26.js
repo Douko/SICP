b3f97f5c681366175b7853f8b96fdfb301ad292e
Douko <bryanpq3237@gmail.com>
2023-07-30 10:44:46 -0500

    Update ex 1-26.js

--- Chapter 1/ex 1-26.js
+++ Chapter 1/ex 1-26.js
@@ -23,7 +23,7 @@ function square(n) {
 /*The difference between the first and second version is that
 in the first we are telling to the interpreter that he ha to 
 calculate every term in expmod()*expmod(). While in the
-second form he can let the execution to the code until he
+second form he can let the execution until he
 reach the more depth level of iteration.That means that 
 in frist version he has to calculete n term, he has to do n steps,
 but in the second form he just has to calculate log(n) terms.

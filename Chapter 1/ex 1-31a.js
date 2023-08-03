d5044e9e8d0544932e3662d76c09044113df16b8
Douko <bryanpq3237@gmail.com>
2023-08-02 23:51:07 -0500

    change

--- Chapter 1/ex 1-31a.js
+++ Chapter 1/ex 1-31a.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function product_iter(t, a, b, result){
     return a > b
         ? result
@@ -12,22 +11,12 @@ function square(m){
     return m*m;
 }
 
-product_iter(wallis_term,1,2,1);
-=======
-function product_iter(t, a, b, result){
-    return a > b
-        ? result
-        :product_iter(t, a + 1, b, result * t(a));
+function factorial(n){
+    function term(a){
+        return a;
+    }
+    return product_iter(term,1,n,1);
 }
-
-function wallis_term(n){
-    return square(2*n)/(square(2*n)-1);
-}
-function square(m){
-    return m*m;
-}
-
 display(2*product_iter(wallis_term,1,10,1));
-
 display(2*product_iter(wallis_term,1,100000,1));
->>>>>>> c0d244eb39580809d3d81fcdbe42dc8a8ec3aead
+factorial(5);

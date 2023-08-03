c0d244eb39580809d3d81fcdbe42dc8a8ec3aead
Source Academy User <No public email provided>
2023-08-02 23:05:23 -0500

    change

---
+++ Chapter 1/ex 1_31.js
@@ -0,0 +1,16 @@
+function product_iter(t, a, b, result){
+    return a > b
+        ? result
+        :product_iter(t, a + 1, b, result * t(a));
+}
+
+function wallis_term(n){
+    return square(2*n)/(square(2*n)-1);
+}
+function square(m){
+    return m*m;
+}
+
+display(2*product_iter(wallis_term,1,10,1));
+
+display(2*product_iter(wallis_term,1,100000,1));

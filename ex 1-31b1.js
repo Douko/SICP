48210e82384fd7c64cfa48e701046201e78da1ff
Douko <bryanpq3237@gmail.com>
2023-08-02 23:53:28 -0500

    change

--- ex 1-31b1.js
+++ ex 1-31b1.js
@@ -11,13 +11,4 @@ function square(m){
     return m*m;
 }
 
-function factorial(n){
-    function term(a){
-        return a;
-    }
-    return product_iter(term,1,n,1);
-}
-
 display(2*product_iter(wallis_term,1,10,1));
-display(2*product_iter(wallis_term,1,100000,1));
-factorial(5);
